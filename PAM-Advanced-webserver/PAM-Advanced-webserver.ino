75aa9fa500c41d89ca87635c0c52f5ada8683568
PI and more <piandmore@j-tools.net>
2016-10-25 08:54:32 +0200

    Update documentation

--- PAM-Advanced-webserver/PAM-Advanced-webserver.ino
+++ PAM-Advanced-webserver/PAM-Advanced-webserver.ino
@@ -1,16 +1,34 @@
+//
+// The ESP8266WiFi library is needed
+//
 #include <ESP8266WiFi.h>
 
+//
+// If a requested paramter is not found, this is what is returned
+//
 #define PARAMNOTFOUND "XxXxXxX"
 
+//
+// Please fill in your SSID and password
+//
 const char* ssid = "YourSSID";
 const char* password = "YourPassword";
 
+//
+// Initialize the webserver
+//
 WiFiServer server(80);
 
 void setup() {
+  //
+  // Initialize serial output
+  //
   Serial.begin(115200);
   Serial.print("Connecting to ");
   Serial.println(ssid);
+  //
+  // Connect to the wifi and wait for a connection
+  //
   WiFi.begin(ssid,password);
   while (WiFi.status()!=WL_CONNECTED) {
     delay(500);
@@ -18,17 +36,29 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi connected");
+  //
+  // Start the webserver
+  //
   server.begin();
   delay(500);
+  //
+  // Show the IP of the server in serial monitor
+  //
   Serial.println("Web server running. The IP is:");
   Serial.println(WiFi.localIP());
 }
 
+//
+// Given the full request string, return just the URI
+//
 String uriRequested (String reqstr) {
   byte p = reqstr.indexOf("T /")+2;
   return reqstr.substring(p,reqstr.indexOf(" ",p));
 }
 
+//
+// Given the full request string, return the requested page
+//
 String pageRequested (String reqstr) {
   String t = uriRequested(reqstr);
   if (t.indexOf("?")>=0) {
@@ -37,6 +67,10 @@ String pageRequested (String reqstr) {
   return t;
 }
 
+//
+// Given the full request string, return the value of the parameter requested
+// or PARAMNOTFOUND
+//
 String giveUrlParam (String reqstr, String search) {
   String t = uriRequested(reqstr);
   if (t.indexOf("?")>=0) {
@@ -57,16 +91,25 @@ String giveUrlParam (String reqstr, String search) {
 void loop() {
   WiFiClient client = server.available();
   if (client) {
+    //
+    // A web client is asking for a page
+    //
     Serial.println("Page requested");
     boolean lastLineBlank = true;
     String requestString = String(100);
     while (client.connected()) {
+      //
+      // We need to read the request of the client
+      //
       if (client.available()) {
         char c = client.read();
         if (requestString.length()<100) {
           requestString = requestString+c;
         }
         if (c=='\n' && lastLineBlank) {
+          //
+          // We have read the request of the client. Now send the correct page
+          //
           Serial.println(requestString);
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
@@ -74,6 +117,9 @@ void loop() {
           client.println();
           client.println("<!DOCTYPE HTML>");
 
+          //
+          // pr will contain the name of the page
+          //
           String pr = pageRequested(requestString);
           if (pr=="/index.php" || pr=="/") {
             client.println("<html>");
@@ -85,6 +131,10 @@ void loop() {
             client.println("<head></head><body>");
             client.println("<h1>You requested the other page</h1>");
             pr = giveUrlParam(requestString,"id");
+            //
+            // In this page we demo that you can see if someone has put
+            // the id param in the requested url and if so, what the value is
+            //
             if (pr==PARAMNOTFOUND) {
               client.println("You did not want to send your id");
             } else {
@@ -111,4 +161,3 @@ void loop() {
     Serial.println("Page send.");
   }
 }
-
